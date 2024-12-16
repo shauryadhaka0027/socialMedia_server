3ef70d87bce463e6a6ad06441a66c24c5ddbd3cb
shaurya <shauryadhaka3@gmail.com>
2024-12-16 10:21:13 +0530

    fix error in notification

--- controller/notification.js
+++ controller/notification.js
@@ -2,14 +2,28 @@ import { Notification } from "../model/notification.js";
 
 export const getNotifications = async (req, res) => {
   try {
-    const { id } = req.body;
+    // const { id } = req.body;
     
    
-    const notifications = await Notification.find({ postUserId: id })
-      .sort({ createdAt: -1 }) 
-      .limit(4)  
-      .populate('sender')  
-      .exec();  
+    // const notifications = await Notification.find({ postUserId: id })
+    //   .sort({ createdAt: -1 }) 
+    //   .limit(4)  
+    //   .populate('sender')  
+    //   .exec();  
+    const { id, readNotification } = req.body;
+
+
+    const notifications = await Notification.find({ postUserId: id, read: true })
+      .sort({ createdAt: -1 })
+      .populate("sender")
+      .exec();
+
+    if (readNotification) {
+      await Notification.updateMany(
+        { postUserId: id, read: true },
+        { $set: { read: false } }
+      );
+    }
 
     // console.log("Notifications:", notifications);
 
